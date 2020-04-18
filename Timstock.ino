986dac7ba06e2cae15f797d0429ae5f864eb9282
Andreas Brändström <andreas.brandstrom@gmail.com>
2020-04-18 12:18:01 +0200

    buttons uses button historo in the loop scope. Timer button presses trigger timer and timer button pushes are used to calculate how long timer is. Abort button press abort timer and/or timer signal

--- Timstock.ino
+++ Timstock.ino
@@ -2,87 +2,82 @@
 #define B1 2
 #define B2 3
 #define B3 4
-bool ten_sec_alarm;
-bool twenty_sec_alarm;
-unsigned long sound_alarm_at; 
-
+#define MASK 0b11000111
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(B1, INPUT_PULLUP);
   pinMode(B2, INPUT_PULLUP);
   pinMode(B3, INPUT_PULLUP);
   Serial.begin(9600);
-  sound_alarm_at = 0; 
-  ten_sec_alarm = 0;
-  twenty_sec_alarm = 0;
   digitalWrite(LED_BUILTIN, LOW);
 }
 
 void loop() {
-  bool b1 = b1_is_pressed();
-  ten_sec_alarm |= b1;
-  bool b2 = b2_is_pressed();
-  twenty_sec_alarm  |= b2;
-  bool b3 = b3_is_pressed();
+  unsigned long sound_alarm_at = 0;
+  uint8_t b1_history = 0;
+  uint8_t b2_history = 0;
+  uint8_t b3_history = 0;
+    
 
-  if(ten_sec_alarm && (sound_alarm_at == 0)){
-    sound_alarm_at = millis() + 10000;
-    ten_sec_alarm = 0;  
-  }
-   
-  if(twenty_sec_alarm && (sound_alarm_at == 0)){
-    sound_alarm_at = millis() + 20000;
-    twenty_sec_alarm = 0;
-  }
+  while(1) {
+    update_button(&b1_history, B1);
+    update_button(&b2_history, B2);
+    update_button(&b3_history, B3);
+    if(is_button_pressed(&b1_history) || is_button_pressed(&b2_history)){
+      Serial.print((unsigned long) 0 - is_button_down(&b1_history));
+      sound_alarm_at = millis() + 
+                       (10000 & ((unsigned long) 0 - is_button_down(&b1_history))) +
+                       (20000 & ((unsigned long) 0 - is_button_down(&b2_history)));
+    }
 
-  if((millis() > sound_alarm_at) && (sound_alarm_at != 0)){
-    digitalWrite(LED_BUILTIN, HIGH);
-    sound_alarm_at = 0;
-  }
+    if((millis() > sound_alarm_at) && (sound_alarm_at != 0)){
+      digitalWrite(LED_BUILTIN, HIGH);
+      sound_alarm_at = 0;
+    }
 
-  if(digitalRead(LED_BUILTIN) && b3){
-    digitalWrite(LED_BUILTIN, LOW);
-  }
+    if(is_button_pressed(&b3_history)){
+      digitalWrite(LED_BUILTIN, LOW);
+      sound_alarm_at = 0;
+    }
 
-  Serial.print(sound_alarm_at);
-  Serial.print("(");
-  Serial.print(b1);
-  Serial.print(",");
-  Serial.print(b2);
-  Serial.print(",");
-  Serial.print(b3);
-  Serial.print(") ");
-  float mesuredVBat = analogRead(VBATPIN);
-  mesuredVBat *= 2;
-  mesuredVBat *= 3.3;
-  mesuredVBat /= 1024;
-  Serial.print("VBat: "); 
-  Serial.println(mesuredVBat);
+    Serial.print(sound_alarm_at);
+    Serial.print(", ");
+    float mesuredVBat = analogRead(VBATPIN);
+    mesuredVBat *= 2;
+    mesuredVBat *= 3.3;
+    mesuredVBat /= 1024;
+    Serial.print("VBat: "); 
+    Serial.println(mesuredVBat);
+  }
 }
 
-uint8_t b1_is_pressed(void){
-  static uint8_t button_history = 0;
-  return is_pressed(&button_history, B1);
-}
-
-uint8_t b2_is_pressed(void){
-  static uint8_t button_history = 0;
-  return is_pressed(&button_history, B2);
-}
-
-int8_t b3_is_pressed(void){
-  static uint8_t button_history = 0;
-  return is_pressed(&button_history, B3);
-}
-
-uint8_t is_pressed(uint8_t *button_history, uint8_t pin_number){
-  uint8_t pressed = 0;
- 
+void update_button(uint8_t *button_history, const uint8_t pin_number){
   *button_history = *button_history << 1;
   *button_history |= (digitalRead(pin_number) == 0);
-  if((*button_history & 0b11000111) == 0b00000111){
+}
+
+uint8_t is_button_pressed(uint8_t *button_history){
+  uint8_t pressed = 0; 
+  if((*button_history & MASK) == 0b00000111){
     pressed = 1;
     *button_history = 0b11111111;
   }
   return pressed;
+}
+
+uint8_t is_button_released(uint8_t *button_history){
+  uint8_t released = 0; 
+  if((*button_history & MASK) == 0b11000000){
+    released = 1;
+    *button_history = 0b00000000;
+  }
+  return released;
+}
+
+uint8_t is_button_down(uint8_t *button_history){
+  return (*button_history == 0b11111111);
+}
+
+uint8_t is_button_up(uint8_t *button_history){
+  return (*button_history == 0b00000000);
 }
