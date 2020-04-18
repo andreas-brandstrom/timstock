924ccff9d31ea003b50e00049e20a55c2e9052c7
Andreas Brändström <andreas.brandstrom@gmail.com>
2020-04-18 12:40:30 +0200

    has abort button definition. Timer lengths are defined as constants

--- Timstock.ino
+++ Timstock.ino
@@ -1,13 +1,16 @@
 #define VBATPIN A6
+#define BA 4
 #define B1 2
 #define B2 3
-#define B3 4
+const unsigned long T1 = 5000;
+const unsigned long T2 = 10000;
 #define MASK 0b11000111
+
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(B1, INPUT_PULLUP);
   pinMode(B2, INPUT_PULLUP);
-  pinMode(B3, INPUT_PULLUP);
+  pinMode(BA, INPUT_PULLUP);
   Serial.begin(9600);
   digitalWrite(LED_BUILTIN, LOW);
 }
@@ -16,18 +19,18 @@ void loop() {
   unsigned long sound_alarm_at = 0;
   uint8_t b1_history = 0;
   uint8_t b2_history = 0;
-  uint8_t b3_history = 0;
+  uint8_t ba_history = 0;
     
 
   while(1) {
     update_button(&b1_history, B1);
     update_button(&b2_history, B2);
-    update_button(&b3_history, B3);
+    update_button(&ba_history, BA);
     if(is_button_pressed(&b1_history) || is_button_pressed(&b2_history)){
       Serial.print((unsigned long) 0 - is_button_down(&b1_history));
       sound_alarm_at = millis() + 
-                       (10000 & ((unsigned long) 0 - is_button_down(&b1_history))) +
-                       (20000 & ((unsigned long) 0 - is_button_down(&b2_history)));
+                       (T1 & ((unsigned long) 0 - is_button_down(&b1_history))) +
+                       (T2 & ((unsigned long) 0 - is_button_down(&b2_history)));
     }
 
     if((millis() > sound_alarm_at) && (sound_alarm_at != 0)){
@@ -35,7 +38,7 @@ void loop() {
       sound_alarm_at = 0;
     }
 
-    if(is_button_pressed(&b3_history)){
+    if(is_button_pressed(&ba_history)){
       digitalWrite(LED_BUILTIN, LOW);
       sound_alarm_at = 0;
     }
